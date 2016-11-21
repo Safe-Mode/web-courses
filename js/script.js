e888270b534a16802f4b4ef3a8d147d29263c2a1
Sergey <1st_equivalent@mail.ru>
2016-11-21 22:31:41 +0300

    Второй контентный блок закончен

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:false,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 40,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
     });
